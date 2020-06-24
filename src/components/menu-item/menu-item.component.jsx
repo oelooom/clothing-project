74cc4ec73cc076e429d81dcc19e8966f08b899bb
Bahrul Ulum <oelooom@Bahruls-MacBook-Pro.local>
2020-06-24 22:12:06 +0700

    Add route on each menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`menu-item ${size}`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div className='background-image' style={{
             backgroundImage: `url(${imageUrl})`
         }} />
@@ -10,7 +13,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
-    </div >
+    </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
