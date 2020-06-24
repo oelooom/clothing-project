3b0461901935325299398648e9311d1e61bb7f2d
Bahrul Ulum <oelooom@Bahruls-MacBook-Pro.local>
2020-06-25 02:12:00 +0700

    crate signin page, forminput component, and button component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' })
+    }
+
+    handleChange = (event) => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I Already Have an Account</h2>
+                <span>Sign in with email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type='email'
+                        name='email'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        label='Email'
+                        required
+                    />
+
+                    <FormInput
+                        type='password'
+                        name='password'
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        label='Password'
+                        required
+                    />
+
+                    <CustomButton type='submit'>Submit</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
