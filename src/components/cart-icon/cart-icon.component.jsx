1c54f6e169d00f0b251ae75fd0a7e8fc6848f911
Bahrul Ulum <oelooom@Bahruls-MacBook-Pro.local>
2020-06-27 17:46:13 +0700

    updating app with selector library

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
+
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchtoProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchtoProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchtoProps)(CartIcon);
