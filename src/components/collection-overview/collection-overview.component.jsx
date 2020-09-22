5bf1fa8efd106c2a9a10f920c95e586feddb8cfb
Rajawali <oelooom@Bahruls-MacBook-Pro.local>
2020-09-22 15:38:03 +0700

    Creating collection overview component

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './collection-overview.styles.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopItems } from '../../redux/shop/shop.selector';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+const CollectionOveriew = ({ collections }) => (
+    <div className='collection-overview'>
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopItems
+})
+
+export default connect(mapStateToProps)(CollectionOveriew);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => {
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
+    return (
+        <div className='shop-page'>
+            <CollectionOverview />
+        </div>
+    )
 }
 
 export default ShopPage;
