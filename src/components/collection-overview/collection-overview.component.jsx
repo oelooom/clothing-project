cb7a0a65d6b34726683ee3f1ade64b56e91eeb37
Rajawali <oelooom@Bahruls-MacBook-Pro.local>
2020-09-24 12:00:33 +0700

    Creating collection pages

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './collection-overview.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopItems } from '../../redux/shop/shop.selector';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selector';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 const CollectionOveriew = ({ collections }) => (
@@ -16,7 +16,7 @@ const CollectionOveriew = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopItems
+    collections: selectCollectionForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOveriew);
